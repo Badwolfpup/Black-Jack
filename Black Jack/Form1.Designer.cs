d1131816e8fe06602127129975441cd0fd5856c5
Adam Folke <Adam.Folke@hudiksvall.se>
2025-01-05 14:06:48 +0100

    .

--- Black Jack/Form1.Designer.cs
+++ Black Jack/Form1.Designer.cs
@@ -28,85 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bytSpelareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.fyllPåMarkerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hjälpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            menuStrip1 = new MenuStrip();
+            startToolStripMenuItem = new ToolStripMenuItem();
+            bytSpelareToolStripMenuItem = new ToolStripMenuItem();
+            fyllPåMarkerToolStripMenuItem = new ToolStripMenuItem();
+            hjälpToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.startToolStripMenuItem,
-            this.hjälpToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1562, 33);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { startToolStripMenuItem, hjälpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(4, 1, 0, 1);
+            menuStrip1.Size = new Size(1041, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
             // 
             // startToolStripMenuItem
             // 
-            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bytSpelareToolStripMenuItem,
-            this.fyllPåMarkerToolStripMenuItem});
-            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
-            this.startToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
-            this.startToolStripMenuItem.Text = "Start";
+            startToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { bytSpelareToolStripMenuItem, fyllPåMarkerToolStripMenuItem });
+            startToolStripMenuItem.Name = "startToolStripMenuItem";
+            startToolStripMenuItem.Size = new Size(43, 22);
+            startToolStripMenuItem.Text = "Start";
             // 
             // bytSpelareToolStripMenuItem
             // 
-            this.bytSpelareToolStripMenuItem.Name = "bytSpelareToolStripMenuItem";
-            this.bytSpelareToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.bytSpelareToolStripMenuItem.Text = "Byt spelare";
-            this.bytSpelareToolStripMenuItem.Click += new System.EventHandler(this.bytSpelareToolStripMenuItem_Click);
+            bytSpelareToolStripMenuItem.Name = "bytSpelareToolStripMenuItem";
+            bytSpelareToolStripMenuItem.Size = new Size(148, 22);
+            bytSpelareToolStripMenuItem.Text = "Byt spelare";
+            bytSpelareToolStripMenuItem.Click += bytSpelareToolStripMenuItem_Click;
             // 
             // fyllPåMarkerToolStripMenuItem
             // 
-            this.fyllPåMarkerToolStripMenuItem.Name = "fyllPåMarkerToolStripMenuItem";
-            this.fyllPåMarkerToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-            this.fyllPåMarkerToolStripMenuItem.Text = "Fyll på marker";
-            this.fyllPåMarkerToolStripMenuItem.Click += new System.EventHandler(this.fyllPåMarkerToolStripMenuItem_Click);
+            fyllPåMarkerToolStripMenuItem.Name = "fyllPåMarkerToolStripMenuItem";
+            fyllPåMarkerToolStripMenuItem.Size = new Size(148, 22);
+            fyllPåMarkerToolStripMenuItem.Text = "Fyll på marker";
+            fyllPåMarkerToolStripMenuItem.Click += fyllPåMarkerToolStripMenuItem_Click;
             // 
             // hjälpToolStripMenuItem
             // 
-            this.hjälpToolStripMenuItem.Name = "hjälpToolStripMenuItem";
-            this.hjälpToolStripMenuItem.Size = new System.Drawing.Size(69, 29);
-            this.hjälpToolStripMenuItem.Text = "Hjälp";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(327, 218);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            hjälpToolStripMenuItem.Name = "hjälpToolStripMenuItem";
+            hjälpToolStripMenuItem.Size = new Size(47, 22);
+            hjälpToolStripMenuItem.Text = "Hjälp";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1562, 786);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Black Jack by Badwolfpup";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Close_1);
-            this.Load += new System.EventHandler(this.Form1_Load_1);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(1041, 524);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Black Jack by Badwolfpup";
+            FormClosed += Form_Close_1;
+            Load += Form1_Load_1;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -116,6 +101,5 @@
         private ToolStripMenuItem bytSpelareToolStripMenuItem;
         private ToolStripMenuItem fyllPåMarkerToolStripMenuItem;
         private ToolStripMenuItem hjälpToolStripMenuItem;
-        private Label label1;
     }
 }
